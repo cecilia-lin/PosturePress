e170569a0adef1be980b85dfd35b3f0d21039b0b
ashleyychu <ashleyychu@gmail.com>
2025-02-11 19:44:59 -0800

    interaction 1

---
+++ web/global.js
@@ -0,0 +1,30 @@
+let pages = [
+    { url: '', title: 'Dashboard' },
+    { url: 'https://physionet.org/content/pmd/1.0.0/', title: 'Dataset' },
+    { url: 'about/', title: 'About' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const basePath = location.pathname.includes('/web/') ? location.pathname.split('/web/')[0] + '/web/' : '/web/';
+
+for (let p of pages) {
+    let url = p.url.startsWith('http') ? p.url : new URL(p.url, location.origin + basePath).pathname;
+    let title = p.title;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (p.url.startsWith('http')) {
+        a.target = '_blank';
+    }
+
+    let currentPath = location.pathname.replace(/\/$/, '');
+    let linkPath = new URL(a.href, location.origin).pathname.replace(/\/$/, '');
+
+    a.classList.toggle('current', currentPath === linkPath);
+
+    nav.append(a);
+}

ac0eb540688292c8f55dd814a19f16758434e938
folicks <fnajera@ucsd.edu>
2025-02-14 22:00:51 -0800

    current

--- global.js
+++ global.js
@@ -31,3 +31,13 @@ for (let p of pages) {
 
     nav.append(a);
 }
+document.addEventListener("scroll", function () {
+    let titleScreen = document.querySelector(".title-screen");
+    let scrollPosition = window.scrollY;
+  
+    // When the user scrolls past 100px, hide the title screen
+    if (scrollPosition > 10) {
+        titleScreen.classList.add("hide-title");
+    } else {
+        titleScreen.classList.remove("hide-title");
+    }});

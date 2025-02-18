8437602de6fc2b50d2d6bebc16e9aa840ce31454
unknown <linybao1@gmail.com>
2025-02-17 22:03:44 -0800

    style in progress

--- global.js
+++ global.js
@@ -8,26 +8,4 @@ document.addEventListener("scroll", function () {
     } else {
         titleScreen.classList.remove("hide-title");
     }
-let pages = [
-    { url: '', title: 'Dashboard' },
-    { url: 'https://physionet.org/content/pmd/1.0.0/', title: 'Dataset' },
-    { url: 'about/', title: 'About' }
-]});
-
-// let nav = document.createElement('nav');
-// document.body.prepend(nav);
-
-const basePath = location.pathname.includes('/web/') ? location.pathname.split('/web/')[0] + '/web/' : '/web/';
-
-for (let p of pages) {
-    let url = p.url.startsWith('http') ? p.url : new URL(p.url, location.origin + basePath).pathname;
-    let title = p.title;
-
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-
-    if (p.url.startsWith('http')) {
-        a.target = '_blank';
-    };
-  };
+});
